0321552a74c1cbf0ec3c973c88b6a84f5432c498
T.Shimomura <tomoyuki.shimomura@gmail.com>
2018-07-11 09:28:07 +0900

    first commit

---
+++ settings.rb
@@ -0,0 +1,9 @@
+# 各種フォルダ設定
+
+# Unityの、Managed および UnityExtensions があるフォルダ
+#UNITY_DATA_PATH = '/Applications/Unity/Unity.app/Contents/'   # macOS
+UNITY_DATA_PATH = 'C:/Program Files/Unity/Editor/Data/'       # Windows
+
+# ダウンロードした日本語版スクリプトリファレンスの展開先
+UNITY_DOC_SRC_PATH = './UnityDocumentation/ScriptReference/'
+
